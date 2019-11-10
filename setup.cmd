ecd0093aebbab1006ab651909c27c8a52a13d140
pal1000 <liviuprodea@yahoo.com>
2019-11-10 22:36:32 +0200

    Improve elevation routine to cover some edge case environments. Issue reported by @julianxhokaxhiu in #17. Based on #18 by @purifiedh2o

--- setup.cmd
+++ setup.cmd
@@ -1,32 +1,49 @@
-@echo off
+@ECHO OFF
+setlocal EnableDelayedExpansion
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto RestoreCD ) else ( goto getPrivileges )
 
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
+:getPrivileges
+if '%1'=='ELEV' ( goto RestoreCD )
 
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+set "batchPath=%~f0"
+set "batchArgs=ELEV"
 
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
+::Add quotes to the batch path, if needed
+set "script=%0"
+set script=%script:"=%
+IF '%0'=='!script!' ( GOTO PathQuotesDone )
+    set "batchPath=""%batchPath%"""
+:PathQuotesDone
 
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
+::Add quotes to the arguments, if needed.
+:ArgLoop
+IF '%1'=='' ( GOTO EndArgLoop ) else ( GOTO AddArg )
+    :AddArg
+    set "arg=%1"
+    set arg=%arg:"=%
+    IF '%1'=='!arg!' ( GOTO NoQuotes )
+        set "batchArgs=%batchArgs% "%1""
+        GOTO QuotesDone
+        :NoQuotes
+        set "batchArgs=%batchArgs% %1"
+    :QuotesDone
+    shift
+    GOTO ArgLoop
+:EndArgLoop
+
+::Create and run the vb script to elevate the batch file
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "cmd", "/c ""!batchPath! !batchArgs!""", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%windir%\system32\wscript.exe" "%temp%\OEgetPrivileges.vbs"
+exit /B
+
+:RestoreCD
+::Remove the elevation tag and set the correct working directory
+IF '%1'=='ELEV' ( shift /1 )
+endlocal
+cd /d "%~dp0"
 :--------------------------------------
 @TITLE Realtek UAD generic driver setup
 @set srventa=1
