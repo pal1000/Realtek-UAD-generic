pal1000/Realtek-UAD-generic

commit a196668724d33e5a898752d8e8f84a57e1f85c06

Escape from infinite loop with error if Realtek Audio Universal Service fails to terminate

setup.cmd
@@ -68,11 +68,19 @@ cd /d "%~dp0"
 @sc delete RtkAudioUniversalService > nul 2>&1
 @echo Done.
 @echo.
+@set srvkillloop=0
 
 :checkservice
 @set runningservice=0
 @for /f "USEBACKQ" %%a IN (`tasklist /FI "IMAGENAME eq RtkAudUService64.exe"`) do @set /a runningservice+=1 > nul
 @IF %runningservice% GTR 1 echo Terminating per-user instances of Realtek Audio Universal Service...
+@IF %runningservice% GTR 1 set /a srvkillloop+=1
+@IF %srvkillloop% EQU 2 echo.
+@IF %srvkillloop% EQU 2 echo ERROR: Failed to terminate Realtek Audio Universal Service. Something is wrong.
+@IF %srvkillloop% EQU 2 echo Preess any key if you really want to continue.
+@IF %srvkillloop% EQU 2 echo.
+@IF %srvkillloop% EQU 2 pause > nul
+@IF %srvkillloop% EQU 2 GOTO cleandrvstore
 @IF %runningservice% GTR 1 taskkill /f /im RtkAudUService64.exe > nul 2>&1
 @IF %runningservice% GTR 1 echo.
 @IF %runningservice% GTR 1 echo Done.

commit bc2860388dbf6028d494909821ee6d3cfabbb591

Fix segfault when Realtek Audio Universal Service value from registry is missing, regression from a196668724d33e5a898752d8e8f84a57e1f85c06. Reported as #21.

setup.cmd
@@ -51,6 +51,7 @@ cd /d "%~dp0"
 @TITLE Realtek UAD generic driver setup
 @echo Begin uninstalling Realtek UAD driver...
 @echo.
+@set srvkillloop=0
 @SET ERRORLEVEL=0
 @REG QUERY HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v RtkAudUService > nul 2>&1
 @IF ERRORLEVEL 1 GOTO checkservice
@@ -61,7 +62,6 @@ cd /d "%~dp0"
 @sc delete RtkAudioUniversalService > nul 2>&1
 @echo Done.
 @echo.
-@set srvkillloop=0
 
 :checkservice
 @set runningservice=0