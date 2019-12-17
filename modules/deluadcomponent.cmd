137fc933c1e5e2bddf38d5c4eedb7981a4b82a39
pal1000 <liviuprodea@yahoo.com>
2019-12-18 00:28:12 +0200

    Add a development version of open source setup It supports deletion of all side by side generic UAD components from driver store at once

---
+++ modules/deluadcomponent.cmd
@@ -0,0 +1,10 @@
+@setlocal ENABLEDELAYEDEXPANSION
+:delcmploop
+@set pubdrvpos=-1
+@set scandrvpos=-1
+@set exitloop=0
+@for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @IF !exitloop! EQU 0 (set /a scandrvpos+=1&set drvval=%%b&IF /I "!drvval: =!"=="%1" (set pubdrvpos=!scandrvpos!&set exitloop=1))
+@IF NOT !pubdrvpos! EQU -1 set scandrvpos=0
+@IF NOT !pubdrvpos! EQU -1 for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @set /a scandrvpos+=1&set drvval=%%b&IF !scandrvpos! EQU !pubdrvpos! (pnputil /delete-driver !drvval: =! /force /reboot & echo.)
+@IF NOT !pubdrvpos! EQU -1 GOTO delcmploop
+@endlocal
