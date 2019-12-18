b20b615901c9e56652b2e0597d340b359ad6ade5
pal1000 <liviuprodea@yahoo.com>
2019-12-18 11:42:45 +0200

    Setup:Driver delete:Print original INF filename and instance counter

--- modules/deluadcomponent.cmd
+++ modules/deluadcomponent.cmd
@@ -1,10 +1,13 @@
 @setlocal ENABLEDELAYEDEXPANSION
+@set loopcount=0
+
 :delcmploop
 @set pubdrvpos=-1
 @set scandrvpos=-1
 @set exitloop=0
-@for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @IF !exitloop! EQU 0 (set /a scandrvpos+=1&set drvval=%%b&IF /I "!drvval: =!"=="%1" (set pubdrvpos=!scandrvpos!&set exitloop=1))
+@for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @IF !exitloop! EQU 0 (set /a scandrvpos+=1&set drvval=%%b&IF /I "!drvval: =!"=="%1" (set pubdrvpos=!scandrvpos!&set exitloop=1&set /a loopcount+=1))
 @IF NOT !pubdrvpos! EQU -1 set scandrvpos=0
-@IF NOT !pubdrvpos! EQU -1 for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @set /a scandrvpos+=1&set drvval=%%b&IF !scandrvpos! EQU !pubdrvpos! (pnputil /delete-driver !drvval: =! /force /reboot & echo.)
+@IF NOT !pubdrvpos! EQU -1 for /F "USEBACKQ tokens=1,2 delims=:" %%a IN (`pnputil /enum-drivers`) do @set /a scandrvpos+=1&set drvval=%%b&IF !scandrvpos! EQU !pubdrvpos! (echo Removing Realtek UAD component %1 instance #!loopcount!...&pnputil /delete-driver !drvval: =! /force /reboot & echo.)
 @IF NOT !pubdrvpos! EQU -1 GOTO delcmploop
+
 @endlocal
