fdb1632c955bb8dd790d2716531b74618ea81020
pal1000 <liviuprodea@yahoo.com>
2020-07-05 19:19:06 +0300

    Refactor tool which enables audio driver update via Windows Update Now it reverts device installation restricttion by hardware ID See #45

--- utility/enablewindowsupdatedriversdownload.cmd
+++ utility/enablewindowsupdatedriversdownload.cmd
@@ -7,10 +7,29 @@
 @exit
 )
 :--------------------------------------
-@CMD /C EXIT 0
-@REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ExcludeWUDriversInQualityUpdate >nul 2>&1
-@if "%ERRORLEVEL%"=="0" (
+@set blockhdaud=0
+@set blockintaud=0
 @echo Enabling drivers download via Windows update...
-@REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ExcludeWUDriversInQualityUpdate /f
+
+@CMD /C EXIT 0
+@REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs /d /e /f HDAUDIO^\FUNC_01^&VEN_10EC >nul 2>&1
+@if "%ERRORLEVEL%"=="0" set blockhdaud=1
+
+@CMD /C EXIT 0
+@REG QUERY HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs /d /e /f INTELAUDIO^\FUNC_01^&VEN_10EC >nul 2>&1
+@if "%ERRORLEVEL%"=="0" set blockintaud=1
+
+@if %blockhdaud% EQU 1 set exitloop=1
+@if %blockhdaud% EQU 1 FOR /F "tokens=1 skip=2 USEBACKQ" %%a IN (`REG QUERY HKEY_LOCAL_MACHINE^\SOFTWARE^\Policies^\Microsoft^\Windows^\DeviceInstall^\Restrictions^\DenyDeviceIDs /d /e /f HDAUDIO^^^\FUNC_01^^^&VEN_10EC 2^>^&1`) do @IF defined exitloop (
+set "exitloop="
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs /v %%a /f
 )
+
+@if %blockintaud% EQU 1 set exitloop=1
+@if %blockintaud% EQU 1 FOR /F "tokens=1 skip=2 USEBACKQ" %%a IN (`REG QUERY HKEY_LOCAL_MACHINE^\SOFTWARE^\Policies^\Microsoft^\Windows^\DeviceInstall^\Restrictions^\DenyDeviceIDs /d /e /f INTELAUDIO^^^\FUNC_01^^^&VEN_10EC 2^>^&1`) do @IF defined exitloop (
+set "exitloop="
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs /v %%a /f
+)
+
+@echo Done.
 @pause
