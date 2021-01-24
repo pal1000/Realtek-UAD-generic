1b745fd261f464fd73f74d2f83ca9dff463eb7f3
pal1000 <liviuprodea@yahoo.com>
2021-01-24 22:03:47 +0200

    Elevation: Support location containing spaces

--- setup.cmd
+++ setup.cmd
@@ -4,7 +4,7 @@
 @CMD /C EXIT 0
 @"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1
 @if NOT "%ERRORLEVEL%"=="0" (
-@powershell -Command Start-Process ""%0"" -Verb runAs 2>nul
+@powershell -Command Start-Process """%0""" -Verb runAs 2>nul
 @exit
 )
 :--------------------------------------
