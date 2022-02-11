pal1000/Realtek-UAD-generic

commit a1a028d4ec24ade48ef38708c22b7ec2226f6c1a

Defeat PNP lockdown using Nircmd

forceupdater/forceupdater.cmd
@@ -38,8 +38,11 @@
 @where /q devcon.exe
 @if NOT "%ERRORLEVEL%"=="0" echo Windows Device console - devcon.exe is required.&pause&exit
 @CMD /C EXIT 0
-@where /q AdvancedRun.exe
-@if NOT "%ERRORLEVEL%"=="0" echo NirSoft AdvancedRun is required.&pause&exit
+@where /q nircmd.exe
+@if NOT "%ERRORLEVEL%"=="0" echo NirSoft CMD is required.&pause&exit
+@CMD /C EXIT 0
+@where /q nircmdc.exe
+@if NOT "%ERRORLEVEL%"=="0" echo NirSoft CMD is required.&pause&exit
 @echo Stopping Windows Audio service to reduce reboot likelihood...
 @echo.
 @net stop Audiosrv /y
@@ -55,7 +58,8 @@
 @IF %advrunworkdir:~0,1%%advrunworkdir:~-1%=="" set advrunworkdir=%advrunworkdir:~1,-1%
 @IF "%advrunworkdir:~-1%"=="\" set advrunworkdir=%advrunworkdir:~0,-1%
 @set advrunworkdir=%advrunworkdir:~0,-13%
-@AdvancedRun.exe /WaitProcess 1 /EXEFilename "%windir%\System32\cmd.exe" /CommandLine "/C forceupdater\defeatpnplock.cmd" /StartDirectory "%advrunworkdir%" /RunAs 4
+@rem AdvancedRun.exe /WaitProcess 1 /EXEFilename "%windir%\System32\cmd.exe" /CommandLine "/C forceupdater\defeatpnplock.cmd" /StartDirectory "%advrunworkdir%" /RunAs 4
+@nircmd.exe elevatecmd runassystem %windir%\System32\cmd.exe /c "%advrunworkdir%\forceupdater\defeatpnplock.cmd"
 @echo Done.
 @echo.
 @IF EXIST patches\*.* echo Applying registry patch...

commit 11c10045f843924af9a504e5740cb8868594821d

Avoid a dangerous race condition in force updater Driver store overwrite and driver re-enablement were racing This regressed in a1a028d4

forceupdater/forceupdater.cmd
@@ -60,7 +60,7 @@
 @set advrunworkdir=%advrunworkdir:~0,-13%
 @rem AdvancedRun.exe /WaitProcess 1 /EXEFilename "%windir%\System32\cmd.exe" /CommandLine "/C forceupdater\defeatpnplock.cmd" /StartDirectory "%advrunworkdir%" /RunAs 4
 @nircmd.exe elevatecmd runassystem %windir%\System32\cmd.exe /c "%advrunworkdir%\forceupdater\defeatpnplock.cmd"
-@echo Done.
+@pause
 @echo.
 @IF EXIST patches\*.* echo Applying registry patch...
 @IF EXIST patches\*.* echo.