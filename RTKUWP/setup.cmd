7ad7448a0ab8743327f19ca0b5558445be4eb9d5
pal1000 <liviuprodea@yahoo.com>
2022-09-14 15:52:07 +0300

    Realtek Audio console setup: XBOX Series X and S support

--- RTKUWP/setup.cmd
+++ RTKUWP/setup.cmd
@@ -1,3 +1,4 @@
 @powershell -NoLogo -Command "Add-AppxPackage -Path *.Appx" 2>nul
 @powershell -NoLogo -Command "Add-AppxPackage -Path *.AppxBundle"
+@powershell -NoLogo -Command "Add-AppxPackage -Path *.EAppxBundle" 2>nul
 @pause
